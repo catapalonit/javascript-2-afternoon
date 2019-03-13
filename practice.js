59e0a087c3e8e9892d6b318bfeaee049ca75d5c8
Bryan Hawkins <bhawkins3729@gmail.com>
2019-03-13 14:25:27 -0500

    finished required questions

--- practice.js
+++ practice.js
@@ -14,7 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name : "Bryan",
+  age : 26
+}
+console.log(me.name)
 
 
 
@@ -25,7 +29,15 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: "The Chariot",
+  food: "Whatever my home chef sent me tonight",
+  person: "My wife",
+  book: "Call me Francis Tucket",
+  movie: "MacGruber",
+  holiday: "Christmas",
+
+}
 
 
 
@@ -33,7 +45,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Toyota 4Runner"
+favoriteThings.brand = "Emerica Skateboarding"
 
 
 
@@ -41,7 +54,8 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets"
+favoriteThings.book = "Harry Potter"
 
 
 
@@ -55,16 +69,20 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+let backPack = {
+  
+}
 
+let item = 'firstPocket'
+
+backPack['firstPocket'] = 'chapstick';
 
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
-
+console.log(backPack)
 
 
 /*
@@ -73,7 +91,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack)
 
 
 
@@ -96,8 +114,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
+user2.name = "Tyler S. McGinnis"
+user2.email = "tyler.mcginnis@devmounta.in"
 
 
 
